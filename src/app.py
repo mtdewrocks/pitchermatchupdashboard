76e29862723fea62084cca5f5cfd223452cbbfee
Shawn Schreier <shawn.schreier13@gmail.com>
2024-05-22 17:20:21 -0500

    Reorder excel reading

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ from dash import Dash, dcc, html, Input, Output, dash_table
 # Preparing your data for usage *******************************************
 print(os.getcwd())
 
+dfGameLogs = pd.read_excel(r"./assets/2024 Pitching Logs.xlsx", usecols=["Name", "Date", "Opp", "W", "L", "IP", "BF", "H", "R", "ER", "HR", "BB", "SO","Pit"])
 df = pd.read_excel(r"https://github.com/mtdewrocks/pitchermatchupdashboard/tree/main/assets/Pitcher_Season_Stats.xlsx", usecols=["Name", "W", "L", "ERA", "IP", "SO", "WHIP", "GS"])
 df['K/IP'] = df["SO"]/df["IP"]
 df['K/IP'] = df['K/IP'].round(2)
